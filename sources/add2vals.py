c0d8160d612a36eee72f5e1eac2cc86de5b7687b
Wahyu Setiawan <wahyusetiawanblog9999@gmail.com>
2023-07-10 23:23:14 +0800

    modify the add2vals inserting number

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -6,11 +6,17 @@ the calc.py library's 'add2' function.
 import sys
 import calc
 
-argnumbers = len(sys.argv) - 1
+print("Please insert the first number:")
+first_number = input().strip()
+
+print("Please insert the second number:")
+second_number = input().strip()
+
+argnumbers = len(first_number.split()) + len(second_number.split())
 
 if argnumbers == 2 :
     print("")
-    print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
+    print("The result is " + str(calc.add2(first_number, second_number)))
     print("")
     sys.exit(0)
 
@@ -18,8 +24,8 @@ if argnumbers != 2 :
     print("")
     print("You entered " + str(argnumbers) + " value/s.")
     print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
-    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-    print("       If unbundled, usage is 'python add2vals.py X Y'.")
+    print("Usage: run add2valsand and insert the first and second number.")
+    print("       If add2vals is not in your path, usage is './add2vals'.")
+    print("       If unbundled, usage is 'python add2vals.py'.")
     print("")
     sys.exit(1)
